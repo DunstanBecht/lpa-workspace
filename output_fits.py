b91531007610d8b86feb1a0cb8f4bb3d16884e4c
Dunstan BECHT <dunstan@becht.network>
2021-08-28 21:41:09 +0200

    add flexibility in fit titles

--- output_fits.py
+++ output_fits.py
@@ -15,26 +15,30 @@ import settings
 print("[OUTPUT FITS]")
 
 for i in range(len(settings.densities_m)):
-    density_csl = notation.quantity(settings.densities_m[i], "m-2")
-    imdir = 'output_data_'+density_csl
+    density_stm = notation.quantity(settings.densities_m[i], "m-2", 'stm')
+    imdir = 'output_data_'+density_stm
     if imdir in os.listdir():
         print("\n"+imdir+":")
-        exdir = 'output_fits_'+density_csl
+        exdir = 'output_fits_'+density_stm
         if not os.path.isdir(exdir):
             os.mkdir(exdir)
         for entry in os.listdir(imdir):
             stm = os.path.splitext(entry)[0]
             if not os.path.isdir(os.path.join(exdir, stm)):
                 print(stm)
-                not_dir = 'notations_'+density_csl
-                with open(os.path.join(not_dir, stm+'.tex')) as f:
-                    title = f.read()
-                    analyze.export(
-                        stm,
-                        imdir=imdir,
-                        exdir=exdir,
-                        title=title,
-                        d=settings.densities[i],
-                    )
+                not_dir = 'notations_'+density_stm
+                ttl_fil = os.path.join(not_dir, stm+'.tex')
+                if os.path.isfile(ttl_fil):
+                    with open(ttl_fil) as f:
+                        title = f.read()
+                else:
+                    title = None
+                analyze.export(
+                    stm,
+                    imdir=imdir,
+                    exdir=exdir,
+                    title=title,
+                    d=settings.densities[i],
+                )
 
 input("\nPress 'enter' to exit...")
