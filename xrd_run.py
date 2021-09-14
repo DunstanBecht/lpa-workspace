85eb79874da33ea49261abafd8145a98af9ca04d
Dunstan BECHT <dunstan@becht.network>
2021-09-15 00:13:26 +0200

    add simulator benchmark

--- xrd_run.py
+++ xrd_run.py
@@ -21,9 +21,9 @@ path = os.getcwd()
 hostname = 'compute-0-1-gpu'
 
 options = { # run options
-    5e13: {'f': round(600/11.8), 'r': 4000},
-    5e14: {'f': round(300/3.7), 'r': 2000},
-    5e15: {'f': round(150/2), 'r': 1000},
+    5e13: {'f': round(600/11.8), 'r': 4000, 'b': 200},
+    5e14: {'f': round(300/3.7), 'r': 2000, 'b': 200},
+    5e15: {'f': round(150/2), 'r': 1000, 'b': 200},
 }
 
 clndir='xrd'
@@ -41,7 +41,13 @@ with pysftp.Connection(hostname, username=username, password=password) as sftp:
     for i in range(len(settings.densities_m)):
         dstcsl = notation.quantity(settings.densities_m[i], "m-2", 'csl')
         dststm = notation.quantity(settings.densities_m[i], "m-2", 'stm')
-        print("\n"+dstcsl+":")
+        f = str(options[settings.densities_m[i]]['f'])
+        r = str(options[settings.densities_m[i]]['r'])
+        b = str(options[settings.densities_m[i]]['b'])
+        print("\n"+dstcsl+":", end=" ")
+        print("Fourier coefficients: "+f, end=", ")
+        print("block repetitions: "+r, end=", ")
+        print("block size: "+b)
         impdir = "input_data_"+dststm
         expdir = "output_data_"+dststm
         if impdir in sftp.listdir(path):
