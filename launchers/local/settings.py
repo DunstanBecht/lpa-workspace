fa9d188e0891b8acabd94f606107671d6e2a357b
Dunstan BECHT <dunstan@becht.network>
2021-12-07 19:33:37 +0100

    change settings

--- launchers/local/settings.py
+++ launchers/local/settings.py
@@ -2,94 +2,46 @@
 # coding: utf-8
 
 """
-Distribution parameter values.
+The studied densities must be extracted from a geometric sequence with 'a'
+(scale factor) the lowest density and 'r' (common ratio) a square number.
+If the common ratio 'r' is of the form 'k**2' then the sizes of the cells
+containing a fixed number 'n' of dislocations follow an arithmetic sequence
+with a common difference of 'k'. This makes it possible to choose the size
+of the crystal which must be a multiple of the LCM of these cell sizes.
+
+Whenever possible, densities with the least number of zeros in scientific
+notation should be chosen.
 """
 
 import numpy as np
-from lpa.input import models, notation
 
-densities_m = [ # studied dislocation densities [m^-2]
-    5e13,
-    5e14,
-    5e15,
-]
+a = 1.250000e+13 # lowest density [m^-2]
+r = 4 # common ratio (choose a square number)
+n = 5 # number of densities generated
+k = np.arange(n)
+rk = r**k
+densities = a*rk # [m^-2]
+side_cells_min = np.sqrt(2/densities)*1e9 # for each density [nm]
+side_roi = 5*side_cells_min[0] # roi size [nm]
+m = [np.arange(int(side_roi/r**(i/2)/side_cells_min[i]))+1 for i in range(n)]
+side_cells = [m[i]*side_cells_min[i] for i in range(n)] # nm
 
-densities = [d*1e-18 for d in densities_m] # [nm^-2]
+# --- help for choosing a and n --------------------------------------------- #
 
-densities_csl = [notation.quantity(d, "m-2", 'csl') for d in densities_m]
-densities_stm = [notation.quantity(d, "m-2", 'stm') for d in densities_m]
-
-def arguments(
-    d: float,
-) -> list:
-    """
-    Return a list of tuples containing the instantiation arguments.
-
-    Input:
-        d (float): dislocation density [nm^-2]
-
-    Output:
-        t (tuple): tuples containing the instantiation arguments
-    """
-
-    # general parameters
-    roisid = 3200 # side of the ROI [nm]
-    bdrcnd = None # boundary conditions
-    intdis = 1/np.sqrt(d) # mean inter dislocation distance [nm]
-
-    # RRDD parameters
-    subsid = ( # subareas side [nm]
-         200,
-         400,
-         800,
-        1600,
-        3200,
-    )
-    modvar1 = (
-        'E',
-        #'R',
-    )
-
-    # RCDD parameters
-    p = 0.2 # ratio of the area occupied by the cell walls
-    q = (1+np.sqrt(1-p))/p # multiplier of wall thickness for cell side
-    k = ( # multiplier of the inter dislocation distance for dipoles
-        0.5,
-        1,
-        2,
-    )
-    modvar2 = (
-        #'E',
-        #'R',
-    )
-    celsid = ( # subareas side [nm]
-        # 200,
-        # 400,
-        # 800,
-        #1600,
-        #3200,
-    )
-    # instantiation arguments
-    prmtup = []
-
-    # RDD arguments
-    #prmtup.append(('square', roisid, models.RDD, {'d': d}, 'edge', bdrcnd))
-
-    # RRDD arguments
-    for modvar in modvar1:
-        for sid in subsid:
-            r = {'d': d, 'v': modvar, 's': sid}
-            prmtup.append(('square', roisid, models.RRDD, r, 'edge', bdrcnd))
-
-    # RCDD arguments
-    for sid in celsid:
-        walthc = round(sid/q) # thickness of the cell walls [nm]
-        diplen = [round(m*intdis) for m in k] # dipole lengths [nm]
-        for modvar in modvar2:
-            r = {'d': d, 'v': modvar, 's': sid, 't': walthc}
-            prmtup.append(('square', roisid, models.RCDD, r, 'edge', bdrcnd))
-        for i in range(len(k)):
-            r = {'d': d, 'v': 'D', 's': sid, 't': walthc, 'l': diplen[i]}
-            prmtup.append(('square', roisid, models.RCDD, r, 'edge', bdrcnd))
-
-    return prmtup
+if __name__ == "__main__":
+    print("d0 must be of form 2/i**2 with i an integer (to have 2 disl/cell)")
+    a_candidates = []
+    for i in range(1, 1000):
+        d0 = (2/i**2)*1e18 # [m^-2]
+        d0str = f"{d0:1.0f}"
+        if len(d0str.strip("0"))<=3: # limit the number of decimal places
+            print(f"\nWith i = {i} -> d0 = {d0:1e}m^-2:")
+            d = d0*rk # [m^-2]
+            s_min = np.sqrt(2/d)*1e9 # [nm]
+            for k in range(n):
+                print(f"d{k} = {d[k]:10.10e}m^-2", end=" ")
+                print(f"s{k}_min = {s_min[k]:15.10f} nm", end="")
+                if int(s_min[k]) == s_min[k]:
+                    print("")
+                else:
+                    print(r" (!)")
