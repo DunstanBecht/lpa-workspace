683ce5b8375934633f7f87b79613d38e1913862d
Dunstan Becht <dunstan@becht.network>
2022-01-07 11:22:14 +0100

    update xrd remote laucher

--- launchers/remote/xrd.py
+++ launchers/remote/xrd.py
@@ -12,17 +12,17 @@ dirloc = f'../local' # directory containing files to be sent
 jobnam = f'xrd' # name of the job
 remdir = f'lpa-{jobnam}-{idf}' # receiving remote directory
 
-with pysftp.Connection(hst, username=usr, password=pwd) as sftp:
-    if not sftp.exists(remdir):
-        sftp.mkdir(remdir)
-    with sftp.cd(remdir):
-        if input("\nSend files ? (y/n) ")=="y":
+if input(f"\nSend {jobnam}.py ? (y/n) ")=="y":
+    with pysftp.Connection(hst, username=usr, password=pwd) as sftp:
+        if not sftp.exists(remdir):
+            sftp.mkdir(remdir)
+        with sftp.cd(remdir):
             for filnam in (f'{jobnam}.py',):
                 send(sftp, filnam, dirloc)
-    print((f"\nRun the following command through ssh:\n"
-           f"ssh compute-0-1-gpu\n"
-           f"module load cuda/10.1\n"
-           f"cd {remdir}\n"
-           f"nohup python3 {jobnam}.py > benchmark.txt &"))
+print((f"\nRun the following command through ssh:\n"
+       f"ssh compute-0-1-gpu\n"
+       f"module load cuda/10.1\n"
+       f"cd {remdir}\n"
+       f"nohup python3 {jobnam}.py > benchmark.txt &"))
 
 input("\nPress 'enter' to exit...")
