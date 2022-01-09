9812e451953fceaa9e9e2a99665ff30e55632b32
Dunstan Becht <dunstan@becht.network>
2022-01-09 12:16:11 +0100

    update fits laucher

--- launchers/local/fits.py
+++ launchers/local/fits.py
@@ -3,14 +3,21 @@
 
 """
 Fit the models on the simulation outputs.
+
+To run in background:
+nohup python3 fits.py <cycle-identifier> > fits.txt &
 """
 
 import numpy as np
 import os
 from lpa.output import analyze
 import cycle
+import sys
 
-cycidf = cycle.select() # cycle identifier
+if len(sys.argv)>1:
+    cycidf = sys.argv[1]
+else:
+    cycidf = cycle.select() # cycle identifier
 cycdir = cycle.directory(cycidf) # cycle directory
 groups = cycle.groups(cycidf, 'outputs') # groups
 
@@ -22,8 +29,7 @@ for group in groups:
         expdir = os.path.join(cycdir, f'fits_{group}')
         if not os.path.isdir(expdir):
             os.makedirs(expdir)
-        for entry in os.listdir(impdir):
-            stm = os.path.splitext(entry)[0]
+        for stm in os.listdir(impdir):
             if not os.path.isdir(os.path.join(expdir, f"{stm}_analysis")):
                 print(stm)
                 ttlfil = f"{stm.replace('_output', '')}.tex"
