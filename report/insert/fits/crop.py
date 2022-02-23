5c7519998130aad3f3020b42959b8064390e569c
Dunstan Becht <dunstan@becht.network>
2022-02-23 15:24:28 +0100

    update fit examples

--- report/insert/fits/crop.py
+++ report/insert/fits/crop.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-with open("output_data_analysis/fits_data_GUW2.dat", 'r') as f:
+with open("output_data_analysis/fits_data_W.dat", 'r') as f:
     content = f.readlines()
 
-with open("fits_data_GUW2.dat", 'w') as f:
-    f.write(''.join(content[:10]))
+with open("croped/fits_data_W.dat", 'w') as f:
+    f.write(''.join(content[:15]))
     f.write('...')
