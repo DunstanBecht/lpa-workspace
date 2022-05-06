7d0f418e1fa44fee916f0b5a378631bb921fc26a
Dunstan Becht <dunstan@becht.network>
2022-05-06 18:44:33 +0200

    update remote xrd

--- launchers/remote/xrd.py
+++ launchers/remote/xrd.py
@@ -12,17 +12,15 @@ dirloc = f'../local' # directory containing files to be sent
 jobnam = f'xrd' # name of the job
 remdir = f'lpa-{jobnam}-{idf}' # receiving remote directory
 
-if input(f"\nSend {jobnam}.py ? (y/n) ")=="y":
+if input(f"\nSend files ? (y/n) ")=="y":
     with pysftp.Connection(hst, username=usr, password=pwd) as sftp:
         if not sftp.exists(remdir):
             sftp.mkdir(remdir)
         with sftp.cd(remdir):
-            for filnam in (f'{jobnam}.py',):
+            for filnam in (f'{jobnam}.py', f'{jobnam}.job'):
                 send(sftp, filnam, dirloc)
 print((f"\nRun the following command through ssh:\n"
-       f"ssh compute-0-1-gpu\n"
-       f"module load cuda/10.1\n"
        f"cd {remdir}\n"
-       f"nohup python3 {jobnam}.py > benchmark.txt &"))
+       f"sbatch {jobnam}.job"))
 
 input("\nPress 'enter' to exit...")
